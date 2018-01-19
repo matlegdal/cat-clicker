51eeafab85a78a018f2763da2090fcff307d72c0
matlegdal <matlegdal@hotmail.com>
2018-01-19 10:11:32 -0500

    add list of images of cats

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 
-var catImg = document.getElementById("myCat");
+var imgObjects = [
+    {
+        src: 'images/cat.jpg',
+        id: 'cat-1',
+        name: 'Baby cat'
+    },
+    {
+        src: 'images/cat2.jpg',
+        id: 'cat-2',
+        name: 'Hiding cat'
+    }
+];
+
+var cats = [];
+
+imgObjects.forEach(function (imgObject) {
+    var image = new Image();
+    image.src = imgObject.src;
+    image.id = imgObject.id;
+    image.classList.add("img-fluid");
+    cats.push(image);
+});
+
+
+
+var catImg = document.getElementById("cat-1");
 var catCounter = 0;
 
 var updateCounter = function () {
