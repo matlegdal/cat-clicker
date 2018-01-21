2c6ca6119f85f55fabbb5212f2fc43d58507a67e
matlegdal <matlegdal@hotmail.com>
2018-01-20 22:13:27 -0500

    refactor with EC6 specs and iffe invocations to eliminate global variables

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
-var listCats = function() {
+(function listCats() {
     cats.forEach(function(cat){
-        var li = document.createElement("li");
+        const li = document.createElement("li");
         li.textContent = cat.name.toString();
         li.classList.add("list-group-item", "cat-list-item");
         li.id = "selector-" + cat.id;
@@ -14,31 +14,30 @@ var listCats = function() {
         document.getElementById("listCats").appendChild(li);
     });
     document.getElementById("selector-"+cats[0].id).classList.add("active")
-};
+})();
 
-var selectCat = function(li) {
-    var items = document.getElementsByClassName("cat-list-item");
-    for (var i = 0; i<items.length; i++) {
-        items[i].classList.remove("active");
-    }
-    li.classList.add("active");
-};
-
-var displayCat = function(cat) {
-    var div = document.getElementById("displayCat");
+function displayCat(cat) {
+    const div = document.getElementById("displayCat");
     div.innerHTML = '';
 
-    var catName = document.createElement("H5");
+    const catName = document.createElement("H5");
     catName.textContent = cat.name;
     div.appendChild(catName);
 
     div.appendChild(cat);
 
-    var catLikes = document.createElement("p");
+    const catLikes = document.createElement("p");
     catLikes.textContent = cat.likes.toString() + " likes for this cat";
     catLikes.id = "likes-"+cat.id;
     div.appendChild(catLikes);
-};
+}
 
-listCats();
+function selectCat(li) {
+    const items = document.getElementsByClassName("cat-list-item");
+    for (let i = 0; i<items.length; i++) {
+        items[i].classList.remove("active");
+    }
+    li.classList.add("active");
+}
+
 displayCat(cats[0]);
--- js/resources.js
+++ js/resources.js
@@ -1,39 +1,42 @@
 
-var cats = [];
-var imgObjects = [
-    {
-        src: 'images/cat.jpg',
-        id: 'cat-1',
-        name: 'Baby cat'
-    },
-    {
-        src: 'images/cat2.jpg',
-        id: 'cat-2',
-        name: 'Hiding cat'
-    },
-    {
-        src: 'images/cat3.jpg',
-        id: 'cat-3',
-        name: 'Loving cat'
-    }
-];
+const cats = (function catLoader () {
+    const cats = [];
+    const imgObjects = [
+        {
+            src: 'images/cat.jpg',
+            id: 'cat-1',
+            name: 'Baby cat'
+        },
+        {
+            src: 'images/cat2.jpg',
+            id: 'cat-2',
+            name: 'Hiding cat'
+        },
+        {
+            src: 'images/cat3.jpg',
+            id: 'cat-3',
+            name: 'Loving cat'
+        }
+    ];
 
-imgObjects.forEach(function (imgObject) {
-    var cat = new Image();
-    cat.src = imgObject.src;
-    cat.id = imgObject.id;
-    cat.classList.add("img-fluid", "rounded");
+    imgObjects.forEach(function (imgObject) {
+        const cat = new Image();
+        cat.src = imgObject.src;
+        cat.id = imgObject.id;
+        cat.classList.add("img-fluid", "rounded");
 
-    cat.likes = 0;
-    cat.name = imgObject.name;
-    cat.addEventListener("click", function () {
-        this.likes++;
-        updateCounter(cat);
-    }, false);
+        cat.likes = 0;
+        cat.name = imgObject.name;
+        cat.addEventListener("click", function () {
+            this.likes++;
+            updateCounter(cat);
+        }, false);
 
-    cats.push(cat);
-});
+        cats.push(cat);
+    });
+    return cats;
+})();
 
-var updateCounter = function (cat) {
+const updateCounter = function (cat) {
     document.getElementById("likes-"+cat.id).textContent=cat.likes.toString() + " likes for this cat";
 };
